1389914c84f99781e7e558ec01196d9e3645869e
shreyanshi1605 <145632760+shreyanshi1605@users.noreply.github.com>
2024-09-06 22:01:36 -0400

    Update SQL Query - Financial Dashboard Data.sql

--- SQL Query - Financial Dashboard Data.sql
+++ SQL Query - Financial Dashboard Data.sql
@@ -1,6 +1,3 @@
--- SQL Query to create and import data from csv files:
-
--- 0. Create a database 
 CREATE DATABASE ccdb;
 
 -- 1. Create cc_detail table
@@ -47,52 +44,47 @@ CREATE TABLE cust_detail (
     Cust_Satisfaction_Score INT
 );
 
+-- 3. Copy csv data into SQL
 
--- 3. Copy csv data into SQL (remember to update the file name and file location in below query)
+SET DATEFORMAT DMY;
 
--- copy cc_detail table
-
-COPY cc_detail
-FROM 'D:\credit_card.csv' 
-DELIMITER ',' 
-CSV HEADER;
-
+BULK INSERT cc_detail
+FROM 'C:\\Users\\SHREYANSHI SHAH\\Downloads\\cc_add.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
 
 -- copy cust_detail table
 
-COPY cust_detail
-FROM 'D:\customer.csv' 
-DELIMITER ',' 
-CSV HEADER;
-
-
-
--- If you are getting below error, then use the below point:  
-   -- ERROR:  date/time field value out of range: "0"
-   -- HINT:  Perhaps you need a different "datestyle" setting.
-
--- Check the Data in Your CSV File: Ensure date column values are formatted correctly and are in a valid format that PostgreSQL can recognize (e.g., YYYY-MM-DD). And correct any incorrect or missing date values in the CSV file. 
-   -- or
--- Update the Datestyle Setting: Set the datestyle explicitly for your session using the following command:
-SET datestyle TO 'ISO, DMY';
-
--- Now, try to COPY the csv files!
-
+BULK INSERT cust_detail
+FROM 'C:\Users\SHREYANSHI SHAH\Downloads\cust_add.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
 
 -- 4. Insert additional data into SQL, using same COPY function
 
 -- copy additional data (week-53) in cc_detail table
 
-COPY cc_detail
-FROM 'D:\cc_add.csv' 
-DELIMITER ',' 
-CSV HEADER;
-
+BULK INSERT cc_detail
+FROM 'C:\\Users\\SHREYANSHI SHAH\\Downloads\\cc_add.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
 
 -- copy additional data (week-53) in cust_detail table (remember to update the file name and file location in below query)
 
-COPY cust_detail
-FROM 'D:\cust_add.csv' 
-DELIMITER ',' 
-CSV HEADER;
+BULK INSERT cc_detail
+FROM 'C:\\Users\\SHREYANSHI SHAH\\Downloads\\cust_add.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
 
